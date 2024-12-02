2cf8b67edb9b26bf4979226578c840f9fe843a43
Stuart <smbkr@users.noreply.github.com>
2024-12-02 16:25:30 +0000

    Day 1, part 2

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 
@@ -6,8 +7,10 @@ fn main() {
     let filename = &args[1];
     let contents = fs::read_to_string(filename).expect("Failed to read file");
     let (mut left, mut right) = parse_input(&contents);
+
     left.sort_unstable();
     right.sort_unstable();
+
     let total_distance: i32 = left
         .iter()
         .zip(right.iter())
@@ -15,6 +18,17 @@ fn main() {
         .sum();
 
     println!("Total distance: {}", total_distance);
+
+    let appearances = count_appearances(&right);
+    let similarity_score: i32 = left
+        .iter()
+        .map(|i| match appearances.get(i) {
+            Some(value) => *value * i,
+            None => 0,
+        })
+        .sum();
+
+    println!("Similarity score: {}", similarity_score);
 }
 
 fn parse_input(input: &String) -> (Vec<i32>, Vec<i32>) {
@@ -33,4 +47,19 @@ fn parse_input(input: &String) -> (Vec<i32>, Vec<i32>) {
     }
 
     (left, right)
+}
+
+fn count_appearances(input: &Vec<i32>) -> HashMap<i32, i32> {
+    let mut appearances: HashMap<i32, i32> = HashMap::new();
+
+    for i in input {
+        match appearances.get_mut(i) {
+            Some(value) => *value += 1,
+            None => {
+                appearances.insert(*i, 1);
+            }
+        }
+    }
+
+    appearances
 }
