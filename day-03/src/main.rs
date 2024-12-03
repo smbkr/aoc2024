8f7cf4ce26e03d27ec7ce2db1a7e53f5aa718736
Stuart <smbkr@users.noreply.github.com>
2024-12-03 14:21:13 +0000

    Day 03, part 2

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -6,14 +6,46 @@ fn main() {
     let file_path = args().skip(1).next().unwrap();
     let contents = fs::read_to_string(&file_path).unwrap();
 
+    let result = simple(&contents);
+    let result2 = with_on_off_operators(&contents);
+
+    println!("simple: {}", result);
+    println!("with operators: {}", result2)
+}
+
+fn simple(input: &str) -> i64 {
     let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
 
     let mut operations: Vec<(i64, i64)> = vec![];
-    for (_, [a, b]) in re.captures_iter(&contents).map(|c| c.extract()) {
+    for (_, [a, b]) in re.captures_iter(input).map(|c| c.extract()) {
         operations.push((a.parse::<i64>().unwrap(), b.parse::<i64>().unwrap()));
     }
 
-    let result = operations.iter().map(|(a, b)| a * b).sum::<i64>();
+    operations.iter().map(|(a, b)| a * b).sum::<i64>()
+}
 
-    println!("{}", result);
+fn with_on_off_operators(input: &str) -> i64 {
+    let re = Regex::new(r"mul\((\d+,\d+)\)|(do\(\))|(don't\(\))").unwrap();
+
+    let mut operations: Vec<(i64, i64)> = vec![];
+    let mut enabled = true;
+
+    for (_, [m]) in re.captures_iter(input).map(|c| c.extract()) {
+        match m {
+            m if m == "do()" => enabled = true,
+            m if m == "don't()" => enabled = false,
+            m => {
+                if enabled {
+                    let parsed = m
+                        .split(',')
+                        .map(|v| v.parse::<i64>().unwrap())
+                        .collect::<Vec<i64>>();
+
+                    operations.push((parsed[0], parsed[1]));
+                }
+            }
+        }
+    }
+
+    operations.iter().map(|(a, b)| a * b).sum::<i64>()
 }
