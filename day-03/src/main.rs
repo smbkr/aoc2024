606abc5233c22eaaa5ce0e6399069bf4c57eb776
Stuart <smbkr@users.noreply.github.com>
2024-12-03 12:58:01 +0000

    Day 03, part 1

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,3 +1,19 @@
+use regex::Regex;
+use std::env::args;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = args().skip(1).next().unwrap();
+    let contents = fs::read_to_string(&file_path).unwrap();
+
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    let mut operations: Vec<(i64, i64)> = vec![];
+    for (_, [a, b]) in re.captures_iter(&contents).map(|c| c.extract()) {
+        operations.push((a.parse::<i64>().unwrap(), b.parse::<i64>().unwrap()));
+    }
+
+    let result = operations.iter().map(|(a, b)| a * b).sum::<i64>();
+
+    println!("{}", result);
 }
