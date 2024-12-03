5bfed74eb5b7802d41313dc5cc587b94acbaaae2
Stuart <smbkr@users.noreply.github.com>
2024-12-03 12:32:24 +0000

    Day 2, part 2

--- day-02/src/report.rs
+++ day-02/src/report.rs
@@ -9,6 +9,35 @@ pub fn check_report(report: &str) -> ReportResult {
         .map(|s| s.parse::<i32>().unwrap())
         .collect::<Vec<_>>();
 
+    // check if original report is safe
+    match check_is_safe(&values) {
+        ReportResult::Safe => return ReportResult::Safe,
+        _ => {}
+    }
+
+    // if not, see if removing any single element makes it safe
+    let permutations = values
+        .iter()
+        .enumerate()
+        .map(|(i, _)| {
+            let mut new = values.clone();
+            new.remove(i);
+
+            new
+        })
+        .collect::<Vec<Vec<_>>>();
+
+    for permutation in permutations {
+        match check_is_safe(&permutation) {
+            ReportResult::Safe => return ReportResult::Safe,
+            _ => continue,
+        }
+    }
+
+    ReportResult::Unsafe
+}
+
+fn check_is_safe(values: &Vec<i32>) -> ReportResult {
     let min_step = 1;
     let max_step = 3;
     let dir = match get_direction(values[0], values[1]) {
@@ -76,16 +105,30 @@ mod tests {
     }
 
     #[test]
-    fn test_check_report_unsafe_inc_and_dec() {
+    fn test_check_report_unsafe_multiple_inc_and_dec() {
         let input = "1 3 2 4 5";
         let result = check_report(input);
         assert!(matches!(result, ReportResult::Unsafe));
     }
 
     #[test]
-    fn test_check_report_unsafe_repeat_value() {
-        let input = "8 6 4 4 1";
+    fn test_check_report_safe_single_inc_and_dec() {
+        let input = "1 3 2 4 5";
         let result = check_report(input);
         assert!(matches!(result, ReportResult::Unsafe));
+    }
+
+    #[test]
+    fn test_check_report_unsafe_multiple_repeat_values() {
+        let input = "8 4 4 4 1";
+        let result = check_report(input);
+        assert!(matches!(result, ReportResult::Unsafe));
+    }
+
+    #[test]
+    fn test_check_report_safe_single_repeat_value() {
+        let input = "8 6 4 4 1";
+        let result = check_report(input);
+        assert!(matches!(result, ReportResult::Safe));
     }
 }
